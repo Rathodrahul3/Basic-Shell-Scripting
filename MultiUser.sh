37dc174b5f03a7c6ad3f57c9d10620d3ed4779d2
Rahul <rathodrahulr@outlook.com>
2023-05-31 15:15:38 +0530

    Final Multiuser script

---
+++ MultiUser.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#####################################################
+## Purpose: To create multipal user from a file    ##
+## Author:  Rahul
+## Enviroment : Admin
+## Date : 02-March-2023
+####################################################
+
+USERF=$1
+
+if [! "$USERF" ]; then
+  echo "Mandatary: Please enter user filename to create users.....Thank You !!!!!!!"
+  exit
+fi
+
+if [! -F "$USERF"]; then
+  echo "The $USERF file is not present"
+  exit
+fi
+
+######### For Loop #################
+for U in `cat $USERF`
+do 
+./Useradd.sh $U
+
+done
